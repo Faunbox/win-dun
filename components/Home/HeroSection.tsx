b6485cc957044b5e91e7a8ebc33621d0deba2967
Faunbox <faunbox2@gmail.com>
2023-09-02 16:54:13 +0200

    hero section animation

--- components/Home/HeroSection.tsx
+++ components/Home/HeroSection.tsx
@@ -9,38 +9,49 @@ const HeroSection = () => {
   return (
     <div className="section-container bg-[url('/images/hero10.jpg')] w-screen h-screen max-h-[1080px] mx-auto bg-right lg:bg-cover bg-no-repeat relative">
       <div className="container flex flex-col md:flex-row justify-center items-start mx-auto align-middle  px-4 py-10 lg:py-0">
-        <motion.aside
-          initial={{
-            opacity: 0,
-            translateY: "-50%",
-            translateX: "-100%",
-          }}
-          whileInView={{
-            opacity: 1,
-            translateY: "-50%",
-            translateX: "-50%",
-          }}
-          exit={{
-            opacity: 0,
-            translateY: "-50%",
-            translateX: "-100%",
-          }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          className="flex flex-col justify-center items-center lg:justify-start w-full lg:w-6/12 mx-auto lg:absolute lg:top-[52%] lg:left-[33%] 2xl:left-[40%] lg:-translate-x-1/2 lg:-translate-y-2/4 -z-1 text-gray-50 mt-4"
-        >
+        <aside className="flex flex-col justify-center items-center lg:justify-start w-full lg:w-6/12 mx-auto lg:absolute lg:top-[52%] lg:left-[33%] 2xl:left-[40%] lg:-translate-x-1/2 lg:-translate-y-2/4 -z-1 text-gray-50 mt-4">
           <div className="flex flex-col">
-            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold px-2 border-black  md:leading-tight">
+            <motion.h1
+              initial={{
+                opacity: 0,
+                translateY: 0,
+              }}
+              animate={{ opacity: [0, 1], translateY: [100, 0] }}
+              transition={{ duration: 0.4, ease: "easeInOut" }}
+              className="text-3xl sm:text-5xl md:text-6xl font-bold px-2 border-black  md:leading-tight"
+            >
               {t("homeDescription1")}
-            </h1>
-            {/* <h2 className="text-md md:text-lg font-light ">
+            </motion.h1>
+            <motion.h1
+              initial={{
+                opacity: 0,
+              }}
+              animate={{ opacity: [0, 1], translateY: [100, 0] }}
+              transition={{ duration: 0.4, ease: "easeInOut", delay: 0.4 }}
+              className="text-3xl sm:text-5xl md:text-6xl font-bold px-2 border-black  md:leading-tight"
+            >
               {t("homeDescription2")}
-            </h2> */}
-            <h2 className="text-xl sm:text-2xl font-light pt-4">
+            </motion.h1>
+            <motion.div
+              initial={{ width: "100%" }}
+              animate={{ width: ["100%", "0%"] }}
+              transition={{ duration: 0.6, ease: "easeInOut", delay: 1.4 }}
+              className="border-b-2 border-white w-full"
+            ></motion.div>
+            <motion.h2
+              initial={{
+                opacity: 0,
+                translateY: -100,
+              }}
+              animate={{ opacity: [0, 1], translateY: [-100, 0] }}
+              transition={{ duration: 0.4, ease: "easeInOut", delay: 0.8 }}
+              className="text-xl sm:text-2xl font-light pt-4"
+            >
               {t("homeDescription3")}
-            </h2>
+            </motion.h2>
           </div>
           <Buttons />
-        </motion.aside>
+        </aside>
         {/* Trapezoid */}
         <div className="h-0 w-10/12 container border-b-white absolute bottom-0 right-[50%] translate-x-1/2 z-10 border-b-[20px] border-r-[30px] border-r-transparent border-l-[30px] border-l-transparent"></div>
       </div>
